5239cbb3918afc10fb6c53b5ffcf7c881fa89051
Nipodemos <nipodemos13@gmail.com>
2022-02-26 21:01:34 -0300

    Configurando o ambiente - webpack dev server

--- chapter 1 - github-explorer/webpack.config.js
+++ chapter 1 - github-explorer/webpack.config.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+  },
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
